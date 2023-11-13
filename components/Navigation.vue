d58b45f4853effaf97872e46ac3c4cb7cc358ff7
fabian <fabian@bigbridge.nl>
2023-11-13 21:36:20 +0100

    added base nav and app layout

---
+++ components/Navigation.vue
@@ -0,0 +1,9 @@
+<template>
+  <nav class="h-screen sticky top-0 left-0 border-r-2">
+    <ul>
+      <li><NuxtLink to="/">Home</NuxtLink></li>
+      <li><NuxtLink to="/meals">Meals</NuxtLink></li>
+      <li><NuxtLink to="/profile">Profile</NuxtLink></li>
+    </ul>
+  </nav>
+</template>
