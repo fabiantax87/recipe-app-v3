715e855883c1a0500eb862e6de45aaa34d67ae28
fabiantax87 <74959529+fabiantax87@users.noreply.github.com>
2023-11-19 19:05:19 +0100

    Feature/add navigation component (#1)
    * added base nav and app layout
    
    * added Inter font
    
    * updated styling for navigation and added profile and my-meals pages
    
    ---------
    
    Co-authored-by: fabian <fabian@bigbridge.nl>

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <NuxtLayout>
+      <div class="grid grid-cols-[350px_1fr]">
+        <Navigation />
+        <NuxtPage />
+      </div>
+    </NuxtLayout>
   </div>
 </template>
---
+++ components/Navigation.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="h-screen sticky top-0 left-0 border-r-2">
+    <ul class="flex flex-col gap-2 p-4 px-6">
+      <NuxtLink to="/">
+        <li class="hover:shadow-hover hover:bg-gray-200 rounded-sm px-1">Home</li>
+      </NuxtLink>
+      <NuxtLink to="/my-meals">
+        <li class="hover:shadow-hover hover:bg-gray-200 rounded-sm px-1">My meals</li>
+      </NuxtLink>
+      <NuxtLink to="/profile">
+        <li class="hover:shadow-hover hover:bg-gray-200 rounded-sm px-1">Profile</li>
+      </NuxtLink>
+    </ul>
+  </nav>
+</template>
